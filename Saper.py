fe438c6144412f01e8bf2822a6bea8f07c1f3b9e
Avsh11 <96619815+Avsh11@users.noreply.github.com>
2023-05-09 15:27:07 +0200

    Update Saper.py
    Dodano odkrywanie terenu mapy

--- Saper.py
+++ Saper.py
@@ -31,7 +31,7 @@ def screenXO(screen):
                "horizontal": "─"        #196
               }
     
-    size = len(screen)                  #rozmiar ekranu
+    size = len(screen)                  #grid_size ekranu
 
     verticalLine = [lines["horizontal"]*3]*size         #lista zawierająca poziome linie
     # print(verticalLine)
@@ -50,16 +50,17 @@ def screenXO(screen):
     for i,row in enumerate(screen):
         printWhite(lines["vertical"])
         for j in row:
-            if j==-1 : printRed("-1 ")
-            elif j==1 : printGreen(" 1 ")
-            elif j==2 : printGreen(" 2 ")
-            elif j==3 : printGreen(" 3 ")
-            elif j==4 : printGreen(" 4 ")
-            elif j==5 : printGreen(" 5 ")
-            elif j==6 : printGreen(" 6 ")
-            elif j==7 : printGreen(" 7 ")
-            elif j==8 : printGreen(" 8 ")
-            elif j==0: printWhite(" 0 ")
+            if j == "" : printRed("   ")
+            elif j == -1 : printRed(" ⚑ ")
+            elif j == 0: printWhite(" 0 ")
+            elif j == 1 : printGreen(" 1 ")
+            elif j == 2 : printGreen(" 2 ")
+            elif j == 3 : printGreen(" 3 ")
+            elif j == 4 : printGreen(" 4 ")
+            elif j == 5 : printGreen(" 5 ")
+            elif j == 6 : printGreen(" 6 ")
+            elif j == 7 : printGreen(" 7 ")
+            elif j == 8 : printGreen(" 8 ")
             else: printWhite("   ")
             printWhite(lines["vertical"])            
         print()
@@ -67,63 +68,72 @@ def screenXO(screen):
 
     printWhite(corners["bottomLeft"]+verticalDown+corners["bottomRight"]+"\n")
 
-
+def openField(x, y):
+    if grid_opened[y][x] == "":
+        if grid_data[y][x] == 0:
+            grid_opened[y][x] = grid_data[y][x]
+            if x != 0: openField(x - 1, y)
+            if x != grid_size - 1: openField(x + 1, y)
+            if y != 0: openField(x, y - 1)
+            if y != grid_size - 1: openField(x, y + 1)
+        elif grid_data[y][x] > 0:
+            grid_opened[y][x] = grid_data[y][x]
+        elif grid_data[y][x] == -1:
+            grid_opened[y][x] = grid_data[y][x]
 
 if __name__ == "__main__":
 
-    rozmiar = 10
-    ilosc_bomb = 50
-    dane = []
+    grid_size = 10
+    bombs_amount = 10
+    grid_data = []
+    grid_opened = []
     
-    for i in range(rozmiar):
-        kolumna = [0 for i in range(rozmiar)]
-        dane.append(kolumna)
+    for i in range(grid_size):
+        row = [0 for i in range(grid_size)]
+        grid_data.append(row)
+     
+    for i in range(grid_size):
+        row = ["" for i in range(grid_size)]
+        grid_opened.append(row)
     
     i = 0
     while True:
-        bomba_x = randint(0, rozmiar-1)
-        bomba_y = randint(0, rozmiar-1)
-        if dane[bomba_y][bomba_x] == 0:
-            dane[bomba_y][bomba_x] = -1
+        bomb_x = randint(0, grid_size - 1)
+        bomb_y = randint(0, grid_size - 1)
+        if grid_data[bomb_y][bomb_x] != -1:
+            grid_data[bomb_y][bomb_x] = -1
             i+=1
-        if i==ilosc_bomb:
+        if i == bombs_amount:
             break
     
-    dane_y = 0
-    while dane_y<rozmiar:
-        dane_x = 0
-        while dane_x<rozmiar:
-            ilosc_obszar_bomb = 0
-            if dane[dane_y][dane_x] == 0:
-                if dane_y != 0 and dane[dane_y - 1][dane_x] == -1:
-                    ilosc_obszar_bomb+=1
-                if dane_y != rozmiar - 1 and dane[dane_y + 1][dane_x] == -1:
-                    ilosc_obszar_bomb+=1
-                if dane_x != 0 and dane[dane_y][dane_x - 1] == -1:
-                    ilosc_obszar_bomb+=1
-                if dane_x != rozmiar - 1 and dane[dane_y][dane_x + 1] == -1:
-                    ilosc_obszar_bomb+=1
-                if dane_y != 0 and dane_x != 0 and dane[dane_y - 1][dane_x - 1] == -1:
-                    ilosc_obszar_bomb+=1
-                if dane_y != 0 and dane_x != rozmiar - 1 and dane[dane_y - 1][dane_x + 1] == -1:
-                    ilosc_obszar_bomb+=1
-                if dane_y != rozmiar - 1 and dane_x != 0 and dane[dane_y + 1][dane_x - 1] == -1:
-                    ilosc_obszar_bomb+=1
-                if dane_y != rozmiar - 1 and dane_x != rozmiar - 1 and dane[dane_y + 1][dane_x + 1] == -1:
-                    ilosc_obszar_bomb+=1
-                dane[dane_y][dane_x] = ilosc_obszar_bomb
-            dane_x+=1
-        dane_y+=1
-    
-    # print(dane)
-   
+    grid_y = 0
+    while grid_y<grid_size:
+        grid_x = 0
+        while grid_x<grid_size:
+            bordering_bombs_amount = 0
+            if grid_data[grid_y][grid_x] == 0:
+                if grid_y != 0 and grid_data[grid_y - 1][grid_x] == -1:
+                    bordering_bombs_amount+=1
+                if grid_y != grid_size - 1 and grid_data[grid_y + 1][grid_x] == -1:
+                    bordering_bombs_amount+=1
+                if grid_x != 0 and grid_data[grid_y][grid_x - 1] == -1:
+                    bordering_bombs_amount+=1
+                if grid_x != grid_size - 1 and grid_data[grid_y][grid_x + 1] == -1:
+                    bordering_bombs_amount+=1
+                if grid_y != 0 and grid_x != 0 and grid_data[grid_y - 1][grid_x - 1] == -1:
+                    bordering_bombs_amount+=1
+                if grid_y != 0 and grid_x != grid_size - 1 and grid_data[grid_y - 1][grid_x + 1] == -1:
+                    bordering_bombs_amount+=1
+                if grid_y != grid_size - 1 and grid_x != 0 and grid_data[grid_y + 1][grid_x - 1] == -1:
+                    bordering_bombs_amount+=1
+                if grid_y != grid_size - 1 and grid_x != grid_size - 1 and grid_data[grid_y + 1][grid_x + 1] == -1:
+                    bordering_bombs_amount+=1
+                grid_data[grid_y][grid_x] = bordering_bombs_amount
+            grid_x+=1
+        grid_y+=1
 
-    #gracz = 1
-    #while True:
-    screenXO(dane)
-    #    if gracz == 1: printGreen("Gracz 1\n")
-    #    else: printRed("Gracz 2\n")
-    #    x = int(input("Podaj wsp x: "))
-    #    y = int(input("Podaj wsp y: "))
-    #    dane[y][x] = gracz
-    #    gracz *= -1
+    while True:
+        screenXO(grid_opened)
+        x = int(input("Podaj wsp x: "))
+        y = int(input("Podaj wsp y: "))
+        openField(x, y)
